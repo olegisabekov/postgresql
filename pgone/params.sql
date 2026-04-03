2fc601d442c97243a67f2bb213a528ae9b8e3652
Oleg Isabekov <olegisabekov@gmail.com>
2026-04-03 20:49:48 +0400

    checkpoint

--- pgone/params.sql
+++ pgone/params.sql
@@ -1,17 +1,4 @@
-drop view viud_one_param_date;
-drop view viud_one_param_number;
-drop view viud_one_param_money;
-drop view viud_one_param_string;
-drop function f_add_one_param;
-drop table one_param_date;
-drop table one_param_string;
-drop table one_param_number;
-drop table one_param_money;
-drop table one_param_ext;
-drop table one_param_grp_lev;
-drop table one_param;
-drop table one_namespace;
-drop sequence seq_one_param_group_level;
+\i dropall.sql
 
 create table one_namespace
 (
@@ -24,14 +11,11 @@ create table one_namespace
 create table one_param
 (
   id serial,
-  ons_id smallint,
   name varchar(200),
-  constraint one_param_id_pk primary key ( id ) using index tablespace one_index,
-  constraint one_param_ons_id_fk foreign key ( ons_id ) references one_namespace (id)
+  constraint one_param_id_pk primary key ( id ) using index tablespace one_index
 ) tablespace one_data;
 
-create unique index one_param_opt_name_ons_id_inx on one_param ( ons_id, name ) tablespace one_index;
-create index one_param_name_inx on one_param ( name ) tablespace one_index;
+create unique index one_param_opt_name_ons_id_inx on one_param ( name ) tablespace one_index;
 
 create sequence seq_one_param_group_level start with 1 increment by 1 cache 20;
 
@@ -56,21 +40,43 @@ create trigger ti_one_param_grp_lev
   for each row
     execute function fi_one_param_grp_lev();
 
+create sequence seq_one_param_ext start with 1 increment by 1 cache 20;
+
 create table one_param_ext
 (
-  id serial,
+  id bigint not null,
   op_id integer not null,
+  ons_id smallint not null,
   group_level integer,
   day_create timestamp not null default localtimestamp,
   day_begin date not null,
   day_end date default null,
   constraint one_param_ext_id_pk primary key ( id ) using index tablespace one_index,
   constraint one_param_ext_id_fk foreign key ( op_id ) references one_param (id),
+  constraint one_param_ext_ons_id_fk foreign key ( ons_id ) references one_namespace (id),
   constraint one_param_ext_group_level_fk foreign key ( group_level ) references one_param_grp_lev (id)
 ) tablespace one_data;
 
-create index one_param_ext_opid_inx on one_param_ext( op_id );
+--create index one_param_ext_opid_inx on one_param_ext( op_id );
 create index one_param_ext_dc_inx on one_param_ext( day_create );
+
+create or replace function fi_one_param_ext_id() returns trigger as 
+$$
+begin
+  if( new.id is null )then
+    new.id := nextval( 'seq_one_param_ext' );
+  end if;
+  if( new.day_create is null )then
+    new.day_create := localtimestamp;
+  end if;
+  return new;
+end;
+$$ language plpgsql;
+
+create trigger ti_one_param_ext
+  before insert on one_param_ext
+  for each row
+    execute function fi_one_param_ext_id();
 
 create table one_param_number
 (
@@ -118,17 +124,17 @@ create table one_param_date
 create index one_param_date_id_inx on one_param_date( id ) tablespace one_index;
 create index one_param_date_day_inx on one_param_date((date_trunc('day', value_ts)::date));
 
-create or replace function f_add_one_param( p_ons_id smallint, p_name varchar)
+create or replace function f_add_one_param( p_name varchar)
 returns integer as $$
 declare
   result integer;
 begin
   with inserted as
-	  ( insert into one_param( ons_id, name ) values( p_ons_id, p_name ) on conflict (ons_id, name) do nothing returning id )
+	  ( insert into one_param( name ) values( p_name ) on conflict (name) do nothing returning id )
 	select id into result
 		from inserted
 	union all
-		select id from one_param where ons_id = p_ons_id and name = p_name;
+		select id from one_param where name = p_name;
 	return result;
 end;
 $$ language plpgsql;
@@ -140,8 +146,8 @@ declare
   v_ope_id integer;
 begin
   if( tg_op = 'INSERT' )then
-    v_op_id := f_add_one_param(new.ons_id, new.name);
-    insert into one_param_ext( op_id, group_level, day_begin ) values( v_op_id, new.group_level, current_date ) returning id into v_ope_id;
+    v_op_id := f_add_one_param(new.name);
+    insert into one_param_ext( op_id, ons_id, group_level, day_begin ) values( v_op_id, new.ons_id, new.group_level, current_date ) returning id into v_ope_id;
     insert into one_param_number( id, value_int, value_float ) values( v_ope_id, new.value_int, new.value_float );
     return new;
   elsif( tg_op = 'UPDATE' )then
@@ -160,7 +166,7 @@ language plpgsql;
 create or replace view viud_one_param_number( id, ons_id, name, group_level, value_int, value_float ) as
   select 
    ope.id,
-   op.ons_id,
+   ope.ons_id,
    op.name,
    ope.group_level,
    opn.value_int,
@@ -181,8 +187,8 @@ declare
   v_ope_id integer;
 begin
   if( tg_op = 'INSERT' )then
-    v_op_id := f_add_one_param(new.ons_id, new.name);
-    insert into one_param_ext( op_id, group_level, day_begin ) values( v_op_id, new.group_level, current_date ) returning id into v_ope_id;
+    v_op_id := f_add_one_param(new.name);
+    insert into one_param_ext( op_id, ons_id, group_level, day_begin ) values( v_op_id, new.ons_id, new.group_level, current_date ) returning id into v_ope_id;
     insert into one_param_string values( v_ope_id, new.value_smal, new.value_medium, new.value_big );
     return new;
   elsif( tg_op = 'UPDATE' )then
@@ -201,7 +207,7 @@ language plpgsql;
 create or replace view viud_one_param_string( id, ons_id, name, group_level, value_smal, value_medium, value_big ) as
   select 
    ope.id,
-   op.ons_id,
+   ope.ons_id,
    op.name,
    ope.group_level,
    ops.value_smal,
@@ -223,8 +229,8 @@ declare
   v_ope_id integer;
 begin
   if( tg_op = 'INSERT' )then
-    v_op_id := f_add_one_param(new.ons_id, new.name);
-    insert into one_param_ext( op_id, group_level, day_begin ) values( v_op_id, new.group_level, current_date ) returning id into v_ope_id;
+    v_op_id := f_add_one_param(new.name);
+    insert into one_param_ext( op_id, ons_id, group_level, day_begin ) values( v_op_id, new.ons_id, new.group_level, current_date ) returning id into v_ope_id;
     insert into one_param_money values( v_ope_id, new.value );
     return new;
   elsif( tg_op = 'UPDATE' )then
@@ -242,8 +248,8 @@ language plpgsql;
 
 create or replace view viud_one_param_money( id, ons_id, name, group_level, value ) as
   select 
-   op.id,
-   op.ons_id,
+   ope.id,
+   ope.ons_id,
    op.name,
    ope.group_level,
    opm.value
@@ -265,8 +271,8 @@ declare
   v_ope_id integer;
 begin
   if( tg_op = 'INSERT' )then
-    v_op_id := f_add_one_param(new.ons_id, new.name);
-    insert into one_param_ext( op_id, group_level, day_begin ) values( v_op_id, new.group_level, current_date ) returning id into v_ope_id;
+    v_op_id := f_add_one_param(new.name);
+    insert into one_param_ext( op_id, ons_id, group_level, day_begin ) values( v_op_id, new.ons_id, new.group_level, current_date ) returning id into v_ope_id;
     insert into one_param_date values( v_ope_id, new.value_date, new.value_ts );
     return new;
   elsif( tg_op = 'UPDATE' )then
@@ -284,8 +290,8 @@ language plpgsql;
 
 create or replace view viud_one_param_date( id, ons_id, name, group_level, value_date, value_ts, day ) as
   select 
-   op.id,
-   op.ons_id,
+   ope.id,
+   ope.ons_id,
    op.name,
    ope.group_level,
    opd.value_date,
