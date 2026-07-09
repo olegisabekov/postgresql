7d7513b25c29d98112e05eff4acfacd0bcdfbbd3
Oleg Isabekov <olegisabekov@gmail.com>
2026-07-09 11:59:13 +0400

    checkpoint

--- example/deferrable.sql
+++ example/deferrable.sql
@@ -29,7 +29,8 @@ insert into t_example_deferrable(id, nn, time_create)
 
 alter table t_example_deferrable alter constraint t_example_deferrable_uq deferrable;
 
-alter table t_example_deferrable drop constraint t_example_deferrable_uq, add constraint t_example_deferrable_uq unique (nn) deferrable initially deferred;
+--alter table t_example_deferrable drop constraint t_example_deferrable_uq, add constraint t_example_deferrable_uq unique (nn) deferrable initially deferred;
+alter table t_example_deferrable drop constraint t_example_deferrable_uq, add constraint t_example_deferrable_uq unique (nn) deferrable;
 
 select
   connamespace::regnamespace schema,
@@ -39,8 +40,23 @@ from pg_constraint where contype in ('p', 'u')
   and connamespace::regnamespace::text != 'pg_catalog'
   and conname='t_example_deferrable_uq';
 
-begin isolation level read committed;
 set statement_timeout to '120s';
+/*
+begin isolation level read committed;
+with
+  nn as (
+    select
+      id,
+      row_number() over () n
+    from t_example_deferrable
+    )
+update t_example_deferrable ed set nn = nn.n
+  from nn
+  where ed.id = nn.id;
+commit;
+*/
+
+start transaction deferrable;
 with
   nn as (
     select
